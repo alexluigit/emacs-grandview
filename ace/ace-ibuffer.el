184eea4e726528428ee46fb9ad9fde27b0211b2b
Alex <hellosimon1103@hotmail.com>
2021-03-23 20:38:46 +0800

    ibuffer: improve custom function

--- ace/ace-ibuffer.el
+++ ace/ace-ibuffer.el
@@ -2,20 +2,19 @@
 
 ;;;###autoload
 (defun ace/ibuffer-buffers-major-mode (&optional arg)
-  "Select buffers that match the current buffer's major mode.
-With optional prefix ARG (\\[universal-argument]) produce an
-`ibuffer' filtered accordingly.  Else use standard completion."
+  "Select buffers to switch to. Same as `switch-to-buffer'.
+With optional prefix ARG (\\[universal-argument]) select buffers
+that match the current buffer's major mode."
   (interactive "P")
   (let* ((major major-mode)
          (prompt "Buffers for"))
     (if arg
-        (ibuffer t (format "*%s %s*" prompt major)
-                 (list (cons 'used-mode major)))
-      (switch-to-buffer
-       (read-buffer
-        (format "%s %s:" prompt major) nil t
-        (lambda (pair) ; pair is (name-string . buffer-object)
-          (with-current-buffer (cdr pair) (derived-mode-p major))))))))
+        (switch-to-buffer
+              (read-buffer
+                (format "%s %s:" prompt major) nil t
+                (lambda (pair) ; pair is (name-string . buffer-object)
+                  (with-current-buffer (cdr pair) (derived-mode-p major)))))
+      (switch-to-buffer (read-buffer "Switch to buffer: ")))))
 
 ;;;###autoload
 (defun ace/ibuffer-buffers-vc-root (&optional arg)
