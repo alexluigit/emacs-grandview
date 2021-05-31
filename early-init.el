c626db87cb3ada2c31d3cbf125bb71c8df406f29
Alex <hellosimon1103@hotmail.com>
2021-05-31 20:58:31 +0800

    Configure native-comp feature

--- early-init.el
+++ early-init.el
@@ -6,9 +6,17 @@
 (tool-bar-mode -1)                   ; disable the toolbar
 (tooltip-mode -1)                    ; disable tooltips
 (menu-bar-mode -1)                   ; disable the menu bar
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/"))
+(setq url-history-file (expand-file-name "url/history" user-emacs-directory))
 
 (setq visible-bell nil)              ; set up the visible bell
 (setq use-dialog-box t)              ; only for mouse events
+(setq native-comp-eln-load-path
+      (remove (expand-file-name "eln-cache/" "~/.config/emacs/") native-comp-eln-load-path))
+(defvar comp-deferred-compilation-deny-list nil)
+(push (expand-file-name "eln-cache/" user-emacs-directory) native-comp-eln-load-path)
+(setq native-comp-async-report-warnings-errors 'silent)
+
 (setq use-file-dialog nil)
 (setq inhibit-startup-message t)
 (setq inhibit-startup-screen t)
