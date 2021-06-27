7bc26e75075f714eb1cff4dcec36b9e0439cc1a9
Alex <hellosimon1103@hotmail.com>
2021-06-27 17:35:35 +0800

    Add proxy server for straight.el bootstrapping

--- init.el
+++ init.el
@@ -6,12 +6,13 @@
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
+    (let ((url-proxy-services '(("https" . "127.0.0.1:1088"))))
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp))))
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
