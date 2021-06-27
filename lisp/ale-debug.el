beaa5bf0e8f7d893bd8f86f039da23384ebd737f
Alex <hellosimon1103@hotmail.com>
2021-06-27 19:22:55 +0800

    Add show messages function

--- lisp/ale-debug.el
+++ lisp/ale-debug.el
@@ -1,6 +1,17 @@
+(eval-when-compile (require 'subr-x))
+
 (defun ale/debug-unadvice (sym)
   "Remove all advices from symbol SYM."
   (interactive "aFunction symbol: ")
   (advice-mapc (lambda (advice _props) (advice-remove sym advice)) sym))
+
+(defun ale/debug-show-messages ()
+  "Show empty *messages* buffer."
+  (interactive)
+  (if-let ((win (get-buffer-window "*Messages*")))
+      (delete-window win)
+    (let ((inhibit-read-only t))
+      (display-buffer-in-side-window (get-buffer "*Messages*") '((side . right)))
+      (with-current-buffer "*Messages*" (erase-buffer)))))
 
 (provide 'ale-debug)
