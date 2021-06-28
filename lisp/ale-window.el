ef2c4cecfba3b57435b540c6dddf4ba2dc4bd9f1
Alex <hellosimon1103@hotmail.com>
2021-06-29 00:11:20 +0800

    Fix wrong format

--- lisp/ale-window.el
+++ lisp/ale-window.el
@@ -1,7 +1,9 @@
 (defun ale/window-split-right ()
+  (interactive)
   (split-window-right) (other-window 1))
 
 (defun ale/window-split-below ()
+  (interactive)
   (split-window-below) (other-window 1))
 
 (defun ale/window-buffers-major-mode (&optional arg)
