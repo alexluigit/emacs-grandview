ed685aa05c79ed6d6bc440c6ebb3d9561cd3c4b8
Alex <hellosimon1103@hotmail.com>
2021-07-02 14:39:33 +0800

    Add proxy / dimension flag in murl-open

--- lisp/ale-murl.el
+++ lisp/ale-murl.el
@@ -4,12 +4,12 @@
   "Select video or stream to play in mpv."
   (interactive "P")
   (let* ((list-file "/home/alex/.cache/murl/main_list.json")
-         (playlist (append (json-read-file list-file) nil)))
+         (playlist (append (json-read-file list-file) nil))
+         (clip (condition-case nil (current-kill 0 t) (error ""))))
     (unless no-hist
-      (when-let* ((clip (condition-case nil (current-kill 0 t) (error "")))
-                  (remove-syntax (set-text-properties 0 (length clip) nil clip))
-                  (is-url (string-prefix-p "http" clip))
-                  (json (shell-command-to-string (concat "murl json " clip)))
+      (set-text-properties 0 (length clip) nil clip)
+      (when-let* ((is-url (string-prefix-p "http" clip))
+                  (json (shell-command-to-string (concat "murl -P 1088 json '" clip "'")))
                   (valid (string-prefix-p "{" json))
                   (obj (json-read-from-string json)))
         (cl-pushnew obj playlist :test 'equal)
@@ -21,6 +21,6 @@
            (annotation (lambda (s) (marginalia--documentation (funcall get-url s))))
            (cands (ale/minibuffer-append-metadata annotation cands-raw))
            (title (completing-read "murls: " cands)))
-      (call-process "murl" nil 0 nil (funcall get-url title) "true"))))
+      (call-process "murl" nil 0 nil "-P 1088" "-d 25%x25%+2870+10" (funcall get-url title)))))
 
 (provide 'ale-murl)
