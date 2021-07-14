5c337ceade27bb9bd15daff3d10da7cd9608de60
Alex <hellosimon1103@hotmail.com>
2021-07-14 22:58:34 +0800

    Refactor ale/frame-adjust-transparency function

--- lisp/ale-frame.el
+++ lisp/ale-frame.el
@@ -3,11 +3,12 @@
   (cond ((and current-prefix-arg (numberp current-prefix-arg))
          (set-frame-parameter (selected-frame) 'alpha `(,(* 10 percent) . 50)))
         (current-prefix-arg
-         (set-frame-parameter (selected-frame) 'alpha '(96 . 50)))
+         (set-frame-parameter (selected-frame) 'alpha '(80 . 50)))
         (t
-         (let ((opa (car (frame-parameter nil 'alpha))))
-           (if (eq opa 70)
-               (set-frame-parameter (selected-frame) 'alpha '(100 . 50))
-             (set-frame-parameter (selected-frame) 'alpha '(70 . 50)))))))
+         (let ((opa (car (frame-parameter nil 'alpha)))
+               (low 60) (high 100))
+           (if (eq opa low)
+               (set-frame-parameter (selected-frame) 'alpha `(,high . 50))
+             (set-frame-parameter (selected-frame) 'alpha `(,low . 50)))))))
 
 (provide 'ale-frame)
