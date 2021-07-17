d8244c6ba59ff902e9eec77c17676d5424060dde
Alex <hellosimon1103@hotmail.com>
2021-07-17 23:23:45 +0800

    ale-embark: add which key support

--- lisp/ale-embark.el
+++ lisp/ale-embark.el
@@ -11,13 +11,13 @@
 (autoload 'consult-imenu "consult")
 (autoload 'consult-outline "consult")
 
-(defvar ale/embark-extras-become-general-map
+(defvar ale/embark-become-general-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "g") 'consult-grep)
     map)
   "General custom cross-package `embark-become' keymap.")
 
-(defvar ale/embark-extras-become-line-map
+(defvar ale/embark-become-line-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "l") 'consult-line)
     (define-key map (kbd "i") 'consult-imenu)
@@ -29,7 +29,7 @@
 (autoload 'project-switch-to-buffer "project")
 (autoload 'project-find-file "project")
 
-(defvar ale/embark-extras-become-file+buffer-map
+(defvar ale/embark-become-file+buffer-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map embark-become-file+buffer-map)
     (define-key map (kbd "B") 'project-switch-to-buffer)
@@ -40,21 +40,48 @@
 (defvar embark-become-keymaps)
 
 ;;;###autoload
-(define-minor-mode ale/embark-extras-keymaps
+(define-minor-mode ale/embark-keymaps
   "Add or remove keymaps from Embark.
-This is based on the value of `ale/embark-extras-add-keymaps'
+This is based on the value of `ale/embark-add-keymaps'
 and is meant to keep things clean in case I ever wish to disable
 those so-called 'extras'."
   :init-value nil
   :global t
-  (let ((maps '(ale/embark-extras-become-general-map
-                ale/embark-extras-become-line-map
-                ale/embark-extras-become-file+buffer-map)))
-    (if ale/embark-extras-keymaps
+  (let ((maps '(ale/embark-become-general-map
+                ale/embark-become-line-map
+                ale/embark-become-file+buffer-map)))
+    (if ale/embark-keymaps
         (dolist (map maps)
           (cl-pushnew map embark-become-keymaps))
       (setq embark-become-keymaps
             (dolist (map maps)
               (delete map embark-become-keymaps))))))
+
+;;;; which-key integration
+
+(defvar embark-action-indicator)
+(defvar embark-become-indicator)
+(declare-function which-key--show-keymap "which-key")
+(declare-function which-key--hide-popup-ignore-command "which-key")
+
+(defvar ale/embark--which-key-state nil
+  "Store state of Embark's `which-key' hints.")
+
+;;;###autoload
+(defun ale/embark-toggle-which-key ()
+  "Toggle `which-key' hints for Embark actions."
+  (interactive)
+  (if ale/embark--which-key-state
+      (progn
+        (setq embark-action-indicator
+                   (let ((act (propertize "Act" 'face 'highlight)))
+                     (cons act (concat act " on '%s'"))))
+        (setq ale/embark--which-key-state nil))
+    (setq embark-action-indicator
+          (lambda (map _target)
+            (which-key--show-keymap "Embark" map nil nil 'no-paging)
+            #'which-key--hide-popup-ignore-command)
+          embark-become-indicator embark-action-indicator)
+    (setq ale/embark--which-key-state t)))
 
 (provide 'ale-embark)
