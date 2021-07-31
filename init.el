34bd02861e49164e47e7622d12b3a9b6a9a35abb
Alex <hellosimon1103@hotmail.com>
2021-07-31 15:13:40 +0800

    init: use md5 hash to determine tangle or not

--- init.el
+++ init.el
@@ -6,12 +6,26 @@
 
 (require 'ale-gc)
 (require 'ale-package)
+(autoload 'ale/files-read "ale-files")
+
+(defun ale/init-modified-p ()
+  (let* ((init-md5-file (concat user-emacs-directory "init-org.md5"))
+         (init-org-file (concat ale/init-dot-repo "ale.org"))
+         (old-md5 (when (file-exists-p init-md5-file)
+                    (ale/files-read init-md5-file)))
+         (new-md5 (secure-hash 'md5 (ale/files-read init-org-file))))
+    (with-temp-buffer
+      (erase-buffer)
+      (insert new-md5)
+      (write-region (point-min) (point-max) init-md5-file))
+    (not (string= old-md5 new-md5))))
 
 (defun ale/init-load-config ()
   "Tangle (if needed) `ale.org' and load `ale.el'."
   (let ((init-el (concat user-emacs-directory "ale.el"))
         (init-org (concat ale/init-dot-repo "ale.org")))
-    (unless (file-exists-p init-el)
+    (when (or (ale/init-modified-p)
+              (not (file-exists-p init-el)))
       (require 'ob-tangle)
       (org-babel-tangle-file init-org init-el))
     (load-file init-el)))
