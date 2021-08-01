a910d9f79b4ba02d0f2734a848862b44b794428d
Alex <hellosimon1103@hotmail.com>
2021-08-01 15:20:59 +0800

    ale-elisp: ale-debug -> ale-elisp

--- lisp/ale-elisp.el
+++ lisp/ale-elisp.el
@@ -1,20 +1,20 @@
 (eval-when-compile (require 'subr-x))
 
-(defun ale/debug-unadvice (sym)
+(defun ale/elisp-unadvice (sym)
   "Remove all advices from symbol SYM."
   (interactive "aFunction symbol: ")
   (advice-mapc (lambda (advice _props) (advice-remove sym advice)) sym))
 
-(defun ale/debug-show-messages ()
+(defun ale/elisp-show-messages ()
   "Show *Messages* buffer."
   (interactive)
   (if-let ((win (get-buffer-window "*Messages*")))
       (delete-window win)
     (display-buffer-in-side-window (get-buffer "*Messages*") '((side . right)))))
 
-(defun ale/debug-erase-messages ()
+(defun ale/elisp-erase-messages ()
   (interactive)
   (let ((inhibit-read-only t))
     (with-current-buffer "*Messages*" (erase-buffer))))
 
-(provide 'ale-debug)
+(provide 'ale-elisp)
