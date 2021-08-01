983668c1a587f075a666c0eac60b729bd4da3aa9
Alex <hellosimon1103@hotmail.com>
2021-08-01 15:11:38 +0800

    init: add ale/init-build function

--- init.el
+++ init.el
@@ -6,28 +6,25 @@
 
 (require 'ale-gc)
 (require 'ale-package)
-(autoload 'ale/files-read "ale-files")
 
-(defun ale/init-modified-p ()
-  (let* ((init-md5-file (concat user-emacs-directory "init-org.md5"))
-         (init-org-file (concat ale/init-dot-repo "ale.org"))
-         (old-md5 (when (file-exists-p init-md5-file)
-                    (ale/files-read init-md5-file)))
-         (new-md5 (secure-hash 'md5 (ale/files-read init-org-file))))
-    (with-temp-buffer
-      (erase-buffer)
-      (insert new-md5)
-      (write-region (point-min) (point-max) init-md5-file))
-    (not (string= old-md5 new-md5))))
-
-(defun ale/init-load-config ()
-  "Tangle (if needed) `ale.org' and load `ale.el'."
-  (let ((init-el (concat user-emacs-directory "ale.el"))
-        (init-org (concat ale/init-dot-repo "ale.org")))
-    (when (or (ale/init-modified-p)
-              (not (file-exists-p init-el)))
+(defun ale/init-build ()
+  "doc"
+  (require 'ale-files)
+  (let* ((init-org (concat ale/init-dot-repo "ale.org"))
+         (init-el (concat user-emacs-directory "ale.el"))
+         (init-md5 (concat user-emacs-directory "init.md5"))
+         (old-md5 (when (file-exists-p init-md5)
+                    (ale/files-read init-md5)))
+         (new-md5 (secure-hash 'md5 (ale/files-read init-org))))
+    (unless (string= old-md5 new-md5)
+      (with-temp-buffer
+        (erase-buffer)
+        (insert new-md5)
+        (write-region (point-min) (point-max) init-md5))
       (require 'ob-tangle)
-      (org-babel-tangle-file init-org init-el))
-    (load-file init-el)))
+      (org-babel-tangle-file init-org init-el))))
 
-(ale/init-load-config)
+(add-hook 'kill-emacs-hook #'ale/init-build)
+
+(let ((init (concat user-emacs-directory "ale.el")))
+  (when (file-exists-p init) (load-file init)))
