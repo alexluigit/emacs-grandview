eb393d77966c9009bf7792a2543e8582eab2196a
Alex <hellosimon1103@hotmail.com>
2021-08-12 19:56:31 +0800

    rg: remove ale-rg.el

--- lisp/ale-rg.el
+++
@@ -1,8 +0,0 @@
-;;; ale-rg.el -*- lexical-binding: t; -*-
-
-;;;###autoload
-(defun ale/rg-config ()
-  (rg-define-toggle "--context 3" (kbd "C"))
-  (rg-define-toggle "-A 5" (kbd "A")))
-
-(provide 'ale-rg)
