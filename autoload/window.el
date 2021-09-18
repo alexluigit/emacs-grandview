5576b29f8617fc9b34a2533c1f214112a03fe233
Alex <hellosimon1103@hotmail.com>
2021-09-18 14:05:12 +0800

    window: remove ale-window-recenter-mode

--- autoload/window.el
+++ autoload/window.el
@@ -4,39 +4,6 @@
   "Tweaks for windows."
   :group 'windows)
 
-(defcustom ale-window-command-centre-list
-  '(magit-diff-visit-file--internal
-    consult-line
-    consult-mark
-    consult-outline
-    consult-project-imenu
-    consult-compile-error
-    consult-org-heading)
-  "Commands to run `ale-window-jump-recentre-hook'.
-You must restart function `ale-window-set-up-hooks-mode' for
-changes to take effect."
-  :group 'ale-window
-  :type 'list)
-
-(defcustom ale-window-command-top-list '()
-  "Commands to run `ale-window-jump-top-hook'.
-You must restart function `ale-window-set-up-hooks-mode' for
-changes to take effect."
-  :group 'ale-window
-  :type 'list)
-
-;;;###autoload
-(defun ale-window-recentre-top (&rest _)
-  "Reposition at the top and pulse line.
-Add this to a hook, such as `imenu-after-jump-hook'."
-  (recenter 0))
-
-;;;###autoload
-(defun ale-window-recentre-centre (&rest _)
-  "Recentre and pulse line.
-Add this to a hook, such as `imenu-after-jump-hook'."
-  (recenter nil))
-
 ;;;###autoload
 (defun ale-split-window-right ()
   (interactive)
@@ -82,19 +49,3 @@ managers such as DWM, BSPWM refer to this state as 'monocle'."
       (when (window-parameter nil 'window-side) (delete-window))
       (delete-other-windows)
       (switch-to-buffer buf))))
-
-;;;###autoload
-(define-minor-mode ale-window-recenter-mode
-  "Set up hooks for Consult."
-  :init-value nil
-  :global t
-  (if ale-window-recenter-mode
-      (progn
-        (dolist (fn ale-window-command-centre-list)
-          (advice-add fn :after #'ale-window-recentre-centre))
-        (dolist (fn ale-window-command-top-list)
-          (advice-add fn :after #'ale-window-recentre-top)))
-    (dolist (fn ale-window-command-centre-list)
-          (advice-remove fn #'ale-window-recentre-centre))
-    (dolist (fn ale-window-command-top-list)
-          (advice-remove fn #'ale-window-recentre-top))))
