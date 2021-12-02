5685d85fcaa7970f150a30402211d799785f5e6a
Alex <hellosimon1103@hotmail.com>
2021-12-03 00:16:04 +0800

    consult: use consult-ripgrep in large files

--- autoload/consult.el
+++ autoload/consult.el
@@ -1,28 +1,49 @@
 ;;; autoload/consult.el --- -*- lexical-binding: t -*-
 
+(require 'consult)
+(autoload 'consult-org "consult-org")
+
+(defun ale-consult-ripgrep-current-file ()
+  "Call `consult-ripgrep' for the current buffer (a single file)."
+  (interactive)
+  (let ((consult-project-root-function #'ignore)
+        (consult-ripgrep-args
+         (concat "rg "
+                 "--null "
+                 "--line-buffered "
+                 "--color=never "
+                 "--line-number "
+                 "--smart-case "
+                 "--no-heading "
+                 "--max-columns=1000 "
+                 "--max-columns-preview "
+                 "--search-zip "
+                 "--with-filename "
+                 (shell-quote-argument buffer-file-name))))
+    (consult-ripgrep)))
+
 ;;;###autoload
 (defun ale-consult-line-advisor (fn &rest args)
-  "A `consult-line' wrapper.
+  "An advice for `consult-line'.
 
-When in a very large file (total lines > 100000), use a poor man's
-version of `consult-line', otherwise execute it directly."
+When in a very large file (total lines > 100000), call
+`consult-ripgrep' on current file, otherwise execute it
+directly."
   (interactive)
-  (let ((total-lines (bound-and-true-p ale-modeline-total-lines)))
-    (unless total-lines
-      (setq total-lines (int-to-string (count-lines (point-min) (point-max)))))
-    (when (stringp total-lines) (setq total-lines (string-to-number total-lines)))
+  (let ((total-lines (count-lines (point-min) (point-max))))
     (if (> total-lines 100000)
-        (let* ((buffer-content (buffer-substring-no-properties (point-min) (point-max)))
-               (raw-strs (split-string buffer-content "\n" nil))
-               (cand (completing-read "Go: " raw-strs)))
-          (beginning-of-buffer)
-          (search-forward cand))
+        (ale-consult-ripgrep-current-file)
       (apply fn args))))
 
 ;;;###autoload
-(defun ale-consult-switch-buffer-advisor (fn &rest args)
-  (interactive)
-  (if (car args) (apply fn args) (consult-buffer)))
+(defun ale-consult-outline-advisor (fn &rest args)
+  "An advice for `consult-outline'.
+
+When in `org-mode', call `consult-org-heading', otherwise call
+`consult-oulline'."
+  (if (derived-mode-p 'org-mode)
+      (consult-org-heading)
+    (apply fn args)))
 
 ;;;###autoload
 (defun ale-consult-project-root ()
