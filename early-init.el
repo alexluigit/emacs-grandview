5750a1f6c643f5fb8de7bdcfb912c1305220bfdb
Alex <hellosimon1103@hotmail.com>
2022-04-05 00:17:34 +0800

    init: merge default settings into early-init.el

--- early-init.el
+++ early-init.el
@@ -81,6 +81,87 @@
 ;; See also: <https://gnu.emacs.bug.narkive.com/Kbl5Gryo/bug-16737-timed-out-waiting-for-reply-from-selection-owner>
 (setq select-active-regions 'only)
 
+;; Disable backup files
+(setq auto-save-list-file-prefix nil
+      auto-save-default nil
+      make-backup-files nil
+      create-lockfiles nil)
+
+;; Don't ask for whether to follow symlink or not
+(setq vc-follow-symlinks nil)
+
+;; By default, page scrolling should keep the point at the same visual
+;; position, rather than force it to the top or bottom of the
+;; viewport.  This eliminates the friction of guessing where the point
+;; has warped to.
+
+;; As for per-line scrolling, I dislike the default behaviour of
+;; visually re-centring the point: it is too aggressive as a standard
+;; mode of interaction.  With the following =setq-default=, the point
+;; will stay at the top/bottom of the screen while moving in that
+;; direction (use C-l to reposition it).
+(setq scroll-step 1)
+(setq scroll-margin 1)
+(setq scroll-conservatively 101)
+(setq scroll-up-aggressively 0.01)
+(setq scroll-down-aggressively 0.01)
+(setq auto-window-vscroll nil)
+(setq fast-but-imprecise-scrolling nil)
+(setq hscroll-step 1)
+(setq hscroll-margin 1)
+
+;; When you install a package or use the various customisation
+;; interfaces to tweak things to your liking, Emacs will append a
+;; piece of Elisp to your init file. In my experience, this is a
+;; common source of inconsistencies, arising from a conflict between
+;; the user's code and what is stored in that custom snippet.
+
+;; As it does not seem possible to outright disable this behaviour, I
+;; instruct Emacs to place all "custom" code in a temporary file that
+;; never gets loaded. This feels kinda hacky but is better than having
+;; some arbitrary code that you accidentally evaluated from messing up
+;; with your carefully designed (and version-controlled)
+;; configuration.
+(put 'list-timers 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
+(setq custom-file (concat user-emacs-directory "custom.el"))
+
+;; I believe tabs, in the sense of inserting the tab character, are
+;; best suited for indentation.  While spaces are superior at
+;; precisely aligning text.  However, I understand that elisp uses its
+;; own approach, which I do not want to interfere with.  Also, Emacs
+;; tends to perform alignments by mixing tabs with spaces, which /can
+;; actually lead to misalignments/ depending on certain variables such
+;; as the size of the tab.  As such, I am disabling tabs by default.
+
+;; If there ever is a need to use different settings in other modes,
+;; we can customise them via hooks.  This is not an issue I have
+;; encountered yet and am therefore refraining from solving a problem
+;; that does not affect me.
+
+;; Note that `tab-always-indent' will first do indentation and then
+;; try to complete whatever you have typed in.
+(setq-default tab-always-indent t)
+(setq-default tab-first-completion 'word-or-paren-or-punct) ; Emacs 27
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+;; Show current key strokes in echo area after 0.25s
+(setq echo-keystrokes 0.25)
+
+;; Do not display continuation lines
+(setq-default truncate-lines t)
+
+;; Sentence settings
+(setq sentence-end-double-space t)
+(setq sentence-end-without-period nil)
+
+;; y,n for yes,no
+(if (<= emacs-major-version 28)
+    (defalias 'yes-or-no-p 'y-or-n-p)
+  (setq use-short-answers t))
+
 (tool-bar-mode -1)                    ; Disable toolbar
 (tooltip-mode -1)                     ; Disable tooltips
 (menu-bar-mode -1)                    ; Disable menu bar
--- init/options.el
+++ init/options.el
@@ -1,82 +1,99 @@
-;;; init/options.el --- -*- lexical-binding: t -*-
+;;; init/options.el -*- lexical-binding: t; -*-
 
-;; Disable backup files
-(setq auto-save-list-file-prefix nil
-      auto-save-default nil
-      make-backup-files nil
-      create-lockfiles nil)
+(defconst IS-GUI (or (daemonp) (display-graphic-p)))
+(defconst INIT-DIR (file-name-directory user-init-file))
 
-;; Don't ask for whether to follow symlink or not
-(setq vc-follow-symlinks nil)
+;; No littering
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/"))
 
-;; By default, page scrolling should keep the point at the same visual
-;; position, rather than force it to the top or bottom of the
-;; viewport.  This eliminates the friction of guessing where the point
-;; has warped to.
+(defvar ale-debug-p nil)
+(defvar ale-cache-dir (concat user-emacs-directory "ale/"))
 
-;; As for per-line scrolling, I dislike the default behaviour of
-;; visually re-centring the point: it is too aggressive as a standard
-;; mode of interaction.  With the following =setq-default=, the point
-;; will stay at the top/bottom of the screen while moving in that
-;; direction (use C-l to reposition it).
-(setq scroll-step 1)
-(setq scroll-margin 1)
-(setq scroll-conservatively 101)
-(setq scroll-up-aggressively 0.01)
-(setq scroll-down-aggressively 0.01)
-(setq auto-window-vscroll nil)
-(setq fast-but-imprecise-scrolling nil)
-(setq hscroll-step 1)
-(setq hscroll-margin 1)
+(defvar ale-autoload-default-dir (concat ale-cache-dir "autoload/"))
+(defvar ale-full-config-org (concat INIT-DIR "ale.org"))
+(defvar ale-full-config (concat ale-cache-dir "full.el"))
+(defun ale-minimal-config () (concat ale-cache-dir "minimal.el"))
+(defvar ale-autoload-file (concat ale-cache-dir "autoload.el"))
+(defvar ale-autoload-dirs `(,ale-autoload-default-dir))
 
-;; When you install a package or use the various customisation
-;; interfaces to tweak things to your liking, Emacs will append a
-;; piece of Elisp to your init file. In my experience, this is a
-;; common source of inconsistencies, arising from a conflict between
-;; the user's code and what is stored in that custom snippet.
+(defvar ale-font-size 32)
+(defvar ale-default-font "Victor Mono")
+(defvar ale-fixed-font "Sarasa Mono SC")
+(defvar ale-variable-font "Sarasa Mono SC")
+(defvar ale-zh-font "LXGW WenKai Mono")
+(defvar ale-zh-font-scale 1.0)
 
-;; As it does not seem possible to outright disable this behaviour, I
-;; instruct Emacs to place all "custom" code in a temporary file that
-;; never gets loaded. This feels kinda hacky but is better than having
-;; some arbitrary code that you accidentally evaluated from messing up
-;; with your carefully designed (and version-controlled)
-;; configuration.
-(put 'list-timers 'disabled nil)
-(put 'erase-buffer 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-(setq custom-file (concat user-emacs-directory "custom.el"))
+(defvar ale-gc-cons-threshold 134217728 ; 128mb
+  "The default value to use for `gc-cons-threshold'.
+If you experience freezing, decrease this.  If you experience
+stuttering, increase this.")
 
-;; I believe tabs, in the sense of inserting the tab character, are
-;; best suited for indentation.  While spaces are superior at
-;; precisely aligning text.  However, I understand that elisp uses its
-;; own approach, which I do not want to interfere with.  Also, Emacs
-;; tends to perform alignments by mixing tabs with spaces, which /can
-;; actually lead to misalignments/ depending on certain variables such
-;; as the size of the tab.  As such, I am disabling tabs by default.
+(defcustom ale-date-specifier "%F"
+  "Date specifier for `format-time-string'.
+Used by `ale-insert-date'."
+  :type 'string
+  :group 'ale)
 
-;; If there ever is a need to use different settings in other modes,
-;; we can customise them via hooks.  This is not an issue I have
-;; encountered yet and am therefore refraining from solving a problem
-;; that does not affect me.
+(defcustom ale-time-specifier "%R %z"
+  "Time specifier for `format-time-string'.
+Used by `ale-insert-date'."
+  :type 'string
+  :group 'ale)
 
-;; Note that `tab-always-indent' will first do indentation and then
-;; try to complete whatever you have typed in.
-(setq-default tab-always-indent t)
-(setq-default tab-first-completion 'word-or-paren-or-punct) ; Emacs 27
-(setq-default tab-width 2)
-(setq-default indent-tabs-mode nil)
+(defcustom ale-quit-minor-modes '(org-tree-slide-mode view-mode)
+  "Disable these minor modes when calling `ale-quit'."
+  :type '(repeat symbol)
+  :group 'ale)
 
-;; Show current key strokes in echo area after 0.25s
-(setq echo-keystrokes 0.25)
+(defcustom ale-files-additional-mime
+  '((".ape" . "audio/ape")
+    (".rmvb" . "video/rm")
+    (".f4v" . "video/f4v"))
+  "doc"
+  :group 'files :type '(repeat cons))
 
-;; Do not display continuation lines
-(setq-default truncate-lines t)
+(defcustom ale-files-dir-alist
+  '(((title . "  Photos")       (path . "~/Pictures/"))
+    ((title . "  Videos")       (path . "~/Video/"))
+    ((title . "  Downloads")    (path . "~/Downloads/")))
+  "doc"
+  :group 'files :type '(repeat list))
 
-;; Sentence settings
-(setq sentence-end-double-space t)
-(setq sentence-end-without-period nil)
+(defcustom ale-files-cmd-alist
+  '(("video/" ("floatwin" "-c" "mpv:emacs-mpv" "mpv" "--x11-name=emacs-mpv" "%f"))
+    (("rm" "rmvb") ("floatwin" "-c" "mpv:emacs-mpv" "mpv" "--x11-name=emacs-mpv" "%f")))
+  "doc"
+  :group 'files :type '(alist :value-type ((choice list string) list)))
 
-;; y,n for yes,no
-(if (<= emacs-major-version 28)
-    (defalias 'yes-or-no-p 'y-or-n-p)
-  (setq use-short-answers t))
+(defcustom ale-term-position-alist
+  '((always . ((window-height . 0.4) (side . bottom))))
+  "doc")
+
+(defconst ale-files-dot-repo (getenv "DOTPATH")
+  "doc")
+
+(defcustom ale-local-leader-key "C-x x SPC"
+  "Another Leader key trigger")
+
+;; `project-prefix-map' is available in emacs > 28.
+(if (boundp 'project-prefix-map)
+    (defalias 'project-map project-prefix-map)
+  (define-prefix-command 'project-prefix-map))
+
+(defalias 'tab-map tab-prefix-map)
+(defalias 'register-map ctl-x-r-map)
+
+;; Open files/dirs or operate on files
+(define-prefix-command 'ale-files-map)
+
+;; `mct' is the acronym for "Minibuffer and Completions in Tandem".
+(define-prefix-command 'ale-mct-map)
+
+;; Programming related commands
+(define-prefix-command 'ale-prog-map)
+
+;; Shortcuts for org related commands
+(define-prefix-command 'ale-org-map)
+
+;; Useful utils such as format buffer, set frame opacity, etc.
+(define-prefix-command 'ale-apps-map)
