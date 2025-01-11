d5498937ea00712fc3998eefe2f67cd1f91f50c8
Alex Lu <hellosimon1103@hotmail.com>
2025-01-11 21:26:47 +0800

    docs: add docs for intro / CORE

--- early-init.el
+++ early-init.el
@@ -9,25 +9,28 @@
 
 (setq
  gc-cons-threshold most-positive-fixnum ; Inhibit garbage collection during startup
+ user-emacs-directory (expand-file-name "~/.cache/emacs/") ; No littering
+ custom-file (concat user-emacs-directory "custom.el") ; Place all "custom" code in a temporary file
+ package-user-dir (locate-user-emacs-file "elpa") ; Use `user-emacs-directory' we set above
  package-quickstart nil ; Prevent package.el loading packages prior to their init-file
- package-enable-at-startup t
+ package-enable-at-startup t ; no need to call `package-initialize'
+ package-install-upgrade-built-in nil ; do not update built-in lib
  package-archives
  '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
    ("nongnu" . "https://elpa.nongnu.org/nongnu/")
    ("melpa" . "https://melpa.org/packages/"))
  package-archive-priorities '(("melpa" . 3) ("gnu-elpa" . 2) ("nongnu" . 1))
- package-install-upgrade-built-in nil
  ad-redefinition-action 'accept ; Disable warnings from legacy advice system
  inhibit-startup-message t ; Reduce noise at startup
  inhibit-startup-echo-area-message user-login-name
  inhibit-default-init t
  initial-scratch-message nil
+ inhibit-compacting-font-caches t ; Inhibit frame resizing for performance
  auto-mode-case-fold nil ; Use case-sensitive `auto-mode-alist' for performance
  fast-but-imprecise-scrolling t ; More performant rapid scrolling over unfontified regions
  ffap-machine-p-known 'reject ; Don't ping things that look like domain names
  frame-inhibit-implied-resize t ; Inhibit frame resizing for performance
  idle-update-delay 1.0  ; slow down UI updates down
- inhibit-compacting-font-caches t ; Inhibit frame resizing for performance
  read-process-output-max (* 1024 1024) ; Increase how much is read from processes in a single chunk.
  process-adaptive-read-buffering nil
  redisplay-skip-fontification-on-input t ; Inhibits it for better scrolling performance.
@@ -37,11 +40,10 @@
  create-lockfiles nil ; Disable lockfiles
  make-backup-files nil ; Disable backup files
  vc-follow-symlinks t ; Do not ask about symlink following
- user-emacs-directory (expand-file-name "~/.cache/emacs/") ; No littering
- custom-file (concat user-emacs-directory "custom.el") ; Place all "custom" code in a temporary file
  use-short-answers t ; y/n for yes/no
  ring-bell-function #'ignore ; Do NOT ring the bell
- package-user-dir (locate-user-emacs-file "elpa"))
+ )
+
 (push '(ignore-errors (grandview-setup-literate-file)) safe-local-eval-forms)
 
 (load custom-file 'noerror 'silent)   ; Load user's customization
